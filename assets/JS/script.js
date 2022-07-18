918d3417e1bb7dd9e13c1a8fe90fccd71b911207
ScoBoFoSho <ferrecisco@icloud.com>
2022-07-18 15:59:56 -0400

    function types

---
+++ assets/JS/script.js
@@ -0,0 +1,12 @@
+var loading = document.getElementById("maincontent");
+
+// three different ways to write a function
+function load() {
+  loading.style.backgroundColor = "black";
+}
+
+// var load = function () {};
+
+// const load = () => {};
+
+load();
